fdc4456b1913e11a6acfb2b2611ee961cf9ac141
rhythmpahwa <93522372+rhythmpahwa@users.noreply.github.com>
2023-06-02 20:53:47 +0530

    Add files via upload

---
+++ Leetcode-Reverse-Integers.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int a=0;
+        while(x!=0){
+            int d=x%10;
+            if((a>INT_MAX/10)||(a<INT_MIN/10)){
+                return 0;
+            }
+            a=(10*a)+d;
+            x=x/10;
+
+
+        }
+        return a;
+        
+    }
+};
